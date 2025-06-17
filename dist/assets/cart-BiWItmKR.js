2c89bb9f60a8b79fcd25980643858adb257969f4
Islam rabea <rabeaeslam56@gmail.com>
2025-06-17 19:45:20 +0300

    update project

--- dist/assets/cart-BiWItmKR.js
+++ dist/assets/cart-BiWItmKR.js
@@ -1,12 +1,12 @@
-import{j as r,t as d,b as l,c as w,i as f,f as v,a as k,s as S,e as b,d as y,g as $}from"./search-product-CkJgnzyF.js";import{s as L,i as j,c as n,d as A}from"./show-sweet-alert-CPHbVcik.js";let i=JSON.parse(localStorage.getItem("products"))||[];function p(){document.querySelectorAll(".delete-product").forEach(e=>{e.addEventListener("click",()=>{const s=+e.getAttribute("data-index"),t=+e.getAttribute("data-id");i.splice(s,1),localStorage.setItem("products",JSON.stringify(i)),u(i),p(),L("Removed from wishlist!",t,"#f44336"),r(),d(),l()})})}let c=JSON.parse(localStorage.getItem("products"))||[];function q(){c&&(u(c),r(),d()),p()}let a=document.querySelector("table tbody");function u(o){a.innerHTML=o.map((e,s)=>{let{id:t,title:g,img:x,price:h,quantity:m}=e;return`
+import{j as r,t as d,b as l,c as m,i as w,f,a as v,s as k,e as S,d as b,g as y}from"./search-product-C18CYfkg.js";import{s as $,i as L,c as n,d as j}from"./show-sweet-alert-CPHbVcik.js";let i=JSON.parse(localStorage.getItem("products"))||[];function A(){document.querySelectorAll(".delete-product").forEach(e=>{e.addEventListener("click",()=>{const s=+e.getAttribute("data-index"),t=+e.getAttribute("data-id");i.splice(s,1),localStorage.setItem("products",JSON.stringify(i)),p(i),$("Removed from wishlist!",t,"#f44336"),r(),d(),l()})})}let c=JSON.parse(localStorage.getItem("products"))||[];function q(){c&&(p(c),r(),d(),A())}let a=document.querySelector("table tbody");function p(o){a.innerHTML=o.map((e,s)=>{let{id:t,title:u,img:g,price:x,quantity:h}=e;return`
       <tr id="${t}"
         class="grid grid-cols-6 gap-5 sm:gap-[40px] py-[3px] border-b-[1px] border-[#dfdfdf] items-center text-[14px]"
       >
         <td>
-          <img class="max-w-[55px]" src="${x}" alt="" />
+          <img class="max-w-[55px]" src="${g}" alt="" />
         </td>
-        <td>${g}</td>
-        <td class="price">$${h}</td>
+        <td>${u}</td>
+        <td class="price">$${x}</td>
         <td>
           <div
             class="increment_decrement flex justify-center h-[30px] w-[80px] items-center gap-[8px] bg-white rounded-[30px] py-[10px] px-[10px]"
@@ -21,7 +21,7 @@ import{j as r,t as d,b as l,c as w,i as f,f as v,a as k,s as S,e as b,d as y,g a
               id="product-quantity-${t}"
               class="quantity text-sm text-gray-700"
             >
-              ${m}
+              ${h}
             </p>
             <div
               data-id=${t}
@@ -36,4 +36,4 @@ import{j as r,t as d,b as l,c as w,i as f,f as v,a as k,s as S,e as b,d as y,g a
           <svg class="w-[16px] text-[#F44336] cursor-pointer" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-x-icon lucide-x"><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg>
         </td>
       </tr>
-    `}).join(""),a.addEventListener("click",e=>{let s=e.target.closest(".icon-plus");if(s){let t=+s.getAttribute("data-id");j(t),n(a,t),r(),d()}}),a.addEventListener("click",e=>{let s=e.target.closest(".icon-minus");if(s){let t=+s.getAttribute("data-id");A(t),n(a,t),r(),d()}})}w({icons:f});v();k();S();b();q();l();y();$();
+    `}).join(""),a.addEventListener("click",e=>{let s=e.target.closest(".icon-plus");if(s){let t=+s.getAttribute("data-id");L(t),n(a,t),r(),d()}}),a.addEventListener("click",e=>{let s=e.target.closest(".icon-minus");if(s){let t=+s.getAttribute("data-id");j(t),n(a,t),r(),d()}})}m({icons:w});f();v();k();S();q();l();b();y();
